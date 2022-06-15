d362ea49314de5091644b6a6a92c505ad4407da8
kiptalam joseph <kiptalamjoseph@gmail.com>
2022-06-15 15:06:18 +0300

    lipa na mpesa

--- web/authorization.php
+++ web/authorization.php
@@ -6,7 +6,7 @@ echo "V3o0SkVMVUF2V2sxWGJBc2xuU055YXJaMXB3N3dwOW86Q0pmY3ljNXRHT1ZNbWtZbQ";
 
 
 $ch = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer V3o0SkVMVUF2V2sxWGJBc2xuU055YXJaMXB3N3dwOW86Q0pmY3ljNXRHT1ZNbWtZbQ==']);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer '.$str]);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($ch);
 curl_close($ch);
