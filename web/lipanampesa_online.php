4819eef63c0d119500c5de7388f9c53fae63406a
kiptalam joseph <kiptalamjoseph@gmail.com>
2022-06-15 18:01:13 +0300

    change lipa na mpesa

--- web/lipanampesa_online.php
+++ web/lipanampesa_online.php
@@ -18,7 +18,7 @@ $curl_post_data = array(
     "PhoneNumber"=>  $_POST["phoneA"],
     "CallBackURL"=>  "https://whispering-shelf-32403.herokuapp.com/confirmation.php",
     "AccountReference"=>  "CompanyXLTD",
-    "TransactionDesc"=>  "Payment of X" 
+    "TransactionDesc"=>  "Payment for Parking" 
 );
 
 $data_string = json_encode($curl_post_data);
@@ -31,30 +31,5 @@ $curl_response = curl_exec($curl);
 print_r($curl_response);
 
 echo $curl_response;
-// $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
-// curl_setopt($ch, CURLOPT_HTTPHEADER, [
-//     'Authorization: Bearer kT3F1CB571VrAEuSVzqFAQvGeQw7',
-//     'Content-Type: application/json'
-// ]);
-// curl_setopt($ch, CURLOPT_POST, 1);
-
-// curl_setopt($ch, CURLOPT_POSTFIELDS,{
-//     "BusinessShortCode": 174379,
-//     "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIwNjE1MTMwMTU5",
-//     "Timestamp": "20220615130159",
-//     "TransactionType": "CustomerPayBillOnline",
-//     "Amount": $_POST["amount"],
-//     "PartyA": $_POST["phoneA"],
-//     "PartyB": 174379,
-//     "PhoneNumber": $_POST["phoneA"],
-//     "CallBackURL": "https://whispering-shelf-32403.herokuapp.com/confirmation.php",
-//     "AccountReference": "CompanyXLTD",
-//     "TransactionDesc": "Payment of X" 
-//   });
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// $response     = curl_exec($ch);
-// curl_close($ch);
-
-// echo $response;
 
 ?>
